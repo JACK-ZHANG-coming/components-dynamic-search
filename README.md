a490469be7c802bf10c19c2541b214c66e6a1c26
zhangqiang <zhangq@yyits.cn>
2021-07-24 23:21:09 +0800

    初始化；

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # components01-dynamic-search
 一个自定义搜索组件
+
+# 运行步骤
+cd my-app
+yarn start
---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  Button,
+} from 'antd';
+
+function App() {
+  return (
+    <Button type="primary">高级搜索</Button>
+  );
+}
+
+export default App;
